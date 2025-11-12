1a7ab8425755f9c0d260121565e279141999a0db
thelegend1313 <interiorlegend1313@gmail.com>
2025-11-12 10:06:32 +0700

    h

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,7 +1,7 @@
 // rafce
 import React from 'react'
 import { createBrowserRouter, RouterProvider } from 'react-router-dom'
-import Home from '../pages/home'
+import Home from '../pages/Home'
 import Shop from '../pages/Shop'
 import Cart from '../pages/Cart'
 import MQuatation from '../pages/MQuatation'
